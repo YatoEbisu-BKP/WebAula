5a2ef713ff8c34cbd846eaa3354d102696d27f8d
Yato V. Ebisu <YatoEbisu@gmail.com>
2020-02-26 13:47:01 -0300

    Seeding service

--- WebAula/Startup.cs
+++ WebAula/Startup.cs
@@ -39,15 +39,17 @@ namespace WebAula
             services.AddDbContext<WebAulaContext>(options =>
                    options.UseMySql(Configuration.GetConnectionString("WebAulaContext"), builder =>
                     builder.MigrationsAssembly("WebAula")));
+            services.AddScoped<SeedingService>();
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
