bff84b9ce481ed04d89ae6fcc2ad625122037ef1
Yato V. Ebisu <YatoEbisu@gmail.com>
2020-02-28 14:36:25 -0300

    Update seller and custom service exception, n deu

--- WebAula/Controllers/SellersController.cs
+++ WebAula/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using WebAula.Models;
 using WebAula.Models.ViewModels;
 using WebAula.Services;
+using WebAula.Services.Exceptions;
 
 namespace WebAula.Controllers
 {
@@ -70,6 +71,43 @@ namespace WebAula.Controllers
                 return NotFound();
             }
             return View(obj);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerServices.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            List<Department> departments = _departmentServices.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerServices.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- WebAula/Services/SellerServices.cs
+++ WebAula/Services/SellerServices.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using WebAula.Data;
 using WebAula.Models;
 using Microsoft.EntityFrameworkCore;
+using WebAula.Services.Exceptions;
 
 namespace WebAula.Services
 {
@@ -27,13 +28,29 @@ namespace WebAula.Services
         }
         public Seller FindById(int id)
         {
-           return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id)
         {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+        public void Update(Seller obj)
+        {
+            if (_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id Not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
